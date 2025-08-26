25a79b4cae657bc21974351d81459403f9e061aa
Subhakanta <jenasubhakanta91@gmail.com>
2025-08-27 00:33:56 +0530

    ER- First Commit

---
+++ src/demoJava/Test.java
@@ -0,0 +1,11 @@
+package demoJava;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Print Yes");
+System.out.println("print No");
+	}
+
+}
